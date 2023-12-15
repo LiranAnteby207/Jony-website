e523880a793eb16a86d65e7055dd7fe050e3c83f
Liran Anteby <liranan@post.bgu.ac.il>
2023-12-15 10:42:29 +0200

    build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Grid, GridItem } from "@chakra-ui/react";
+import { useState } from "react";
 
 function App() {
-  return <Button>liran's button</Button>;
+  const [showSidebar, setSideBar] = useState(false);
+  const handleButtonClick = () => {
+    setSideBar(!showSidebar);
+  };
+  const gridTemplateAreas = showSidebar
+    ? `"sidebar   nav" "sidebar main"`
+    : `"nav   nav" "main main"`;
+  return (
+    <Grid templateAreas={gridTemplateAreas} templateColumns="200px 1fr" gap={1}>
+      {showSidebar && (
+        <GridItem area="sidebar" bg="gold" colSpan={1}>
+          {/* Sidebar content goes here */}
+          Sidebar Content
+        </GridItem>
+      )}
+      <GridItem area="nav" bg="coral">
+        <Button onClick={handleButtonClick}>sidebar handle</Button>
+      </GridItem>
+      <GridItem area="main" bg="green">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
