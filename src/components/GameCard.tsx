68d77864129addf34ce7a3db9f0509c1fe15fc3b
Liran Anteby <liranan@post.bgu.ac.il>
2023-12-17 11:48:48 +0200

    handle games with no img

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,7 @@
 import { Card, CardBody, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
-
+import noImage from "../assets/no-image-placeholder-6f3882e0.webp";
 interface Props {
   game: Game;
 }
@@ -9,7 +9,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card>
-      <Image src={game.background_image} />
+      <Image src={game.background_image || noImage} />
       <CardBody>
         <PlatformIconList
           platforms={game.parent_platforms.map((p) => p.platform)}
