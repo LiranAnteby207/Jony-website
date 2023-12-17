eb2032c7af30dba83d8bd04eef031eee52b145d2
Liran Anteby <liranan@post.bgu.ac.il>
2023-12-17 11:21:14 +0200

    genres manu

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Grid, GridItem, HStack } from "@chakra-ui/react";
 import { useState } from "react";
 import NavBar from "./components/NavBar";
 import SideBar from "./components/SideBar";
@@ -6,6 +6,7 @@ import GameGrid from "./components/GameGrid";
 import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
+import GenreSelector from "./components/GenreSelector";
 
 function App() {
   const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(
@@ -25,21 +26,23 @@ function App() {
     <Grid templateAreas={gridTemplateAreas} templateColumns="200px 1fr" gap={2}>
       {showSidebar && (
         <GridItem area="sidebar" colSpan={1} paddingX={5}>
-          <SideBar
-            onSelectGenre={(genre) => setSelectedGenre(genre)}
-            onClick={handleButtonClick}
-            selectedGenre={selectedGenre}
-          />
+          <SideBar onClick={handleButtonClick} />
         </GridItem>
       )}
       <GridItem area="nav">
         <NavBar onClick={handleButtonClick} isNotDisplay={isNotDisplay} />
       </GridItem>
       <GridItem padding={35} area="main">
-        <PlatformSelector
-          selectedPlatform={selectedPlatform}
-          onSelectPlatform={(platform) => setSelectedPlatform(platform)}
-        />
+        <HStack>
+          <GenreSelector
+            selectedGenre={selectedGenre}
+            onSelectGenre={(genre) => setSelectedGenre(genre)}
+          />
+          <PlatformSelector
+            selectedPlatform={selectedPlatform}
+            onSelectPlatform={(platform) => setSelectedPlatform(platform)}
+          />
+        </HStack>
         <GameGrid
           selectedPlatform={selectedPlatform}
           selectedGenre={selectedGenre}
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,20 +1,16 @@
 import { Button, HStack } from "@chakra-ui/react";
 import ColorModeSwitch from "./ColorModeSwitch";
-import GenreList from "./GenreList";
-import { Genre } from "../hooks/useGenres";
+
 interface Props {
   onClick: () => void;
-  onSelectGenre: (genre: Genre) => void;
-  selectedGenre: Genre | null;
 }
-const SideBar = ({ onClick, onSelectGenre, selectedGenre }: Props) => {
+const SideBar = ({ onClick }: Props) => {
   return (
     <>
       <HStack justifyContent="space-between" padding="10px">
         <Button onClick={onClick}>x</Button>
         <ColorModeSwitch />
       </HStack>
-      <GenreList onSelectGenre={onSelectGenre} selectedGenre={selectedGenre} />
     </>
   );
 };
