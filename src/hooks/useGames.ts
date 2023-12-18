46dd9b908c98f7847be433c7f621fb65a1aa49c7
Liran Anteby <liranan@post.bgu.ac.il>
2023-12-18 10:40:41 +0200

    refactor - simplify time calculations

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,6 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
+import ms from 'ms';
 import APIClient, {FetchResponse} from '../services/api-client';
 export interface Platform{
   id:number ;
@@ -28,7 +29,7 @@ useInfiniteQuery<FetchResponse<Game>, Error>({
   getNextPageParam: (lastPage, allPages) => {
     return lastPage.next ? allPages.length + 1: undefined;
   },
-  staleTime: 24*60*60*1000 //24h
+  staleTime: ms('24h')
 });
 
 export default useGames;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,11 +2,12 @@ import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
 import APIClient from '../services/api-client';
 import { Platform } from "./useGames";
+import ms from 'ms';
 const apiClient = new APIClient<Platform>('/platforms/lists/parents');
 const usePlatforms = () => useQuery({
     queryKey: ['platforms'],
     queryFn: apiClient.getAll,
-    staleTime: 24*60*60*1000, //24h
+    staleTime: ms('24h'), 
     initialData: platforms
 });
 export default usePlatforms;
